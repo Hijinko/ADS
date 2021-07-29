de283e232563bfa6ed24ad6dc847bd203edf4da8
Kevin Ellis <kevin.n.ellis3.mil@mail.mil>
2021-07-29 10:26:25 -0400

    Add formulas to get specified members in heap

--- heap/src/heap.c
+++ heap/src/heap.c
@@ -4,17 +4,50 @@
 
 struct heap_node {
     void * p_data;
+    int64_t index; 
 };
 
 struct heap {
     int ordering;
     int max_children;
+    int64_t size;
     void (* destory)(void * p_data);
     int8_t (* compare)(void * p_key1, void * p_key2);
-    hnode ** pp_members;
+    hnode ** pp_array;
 };
 
-heap * heap_init(int ordering, int max_children, void (* destroy)(void * p_data), int8_t (* compare)(void * p_key1, void * p_key2));
+static hnode * heap_member(heap * p_heap, int index)
+{
+    return (hnode *)((p_heap->pp_array) + index);
+}
+
+static hnode * heap_parent(heap * p_heap, hnode * p_node)
+{
+    int index = ((p_node->index) / 2);
+    return heap_member(p_heap, index);
+}
+
+static hnode * heap_tail(heap * p_heap)
+{
+    return heap_member(p_heap, p_heap->size);
+}
+
+static hnode * heap_left(heap * p_heap, hnode * p_node)
+{
+    int index = ((p_node->index) * (p_heap->max_children));
+    return heap_member(p_heap, index);
+}
+
+static hnode * heap_right(heap * p_heap, hnode * p_node)
+{
+    int index = (((p_node->index) * (p_heap->max_children)) + 1);
+    return heap_member(p_heap, index);
+}
+
+heap * heap_init(int ordering, int max_children, void (* destroy)(void * p_data), int8_t (* compare)(void * p_key1, void * p_key2))
+{
+
+}
 void * heap_destroy(heap * p_heap);
 hnode * heap_insert(heap * p_heap, void * p_data);
 hnode * heap_peak(heap * p_heap);
