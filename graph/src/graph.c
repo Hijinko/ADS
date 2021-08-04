5e5c8f8c8a031362aa4345913be1f2bdeae9debc
Kevin Ellis <kevin.n.ellis3.mil@mail.mil>
2021-08-04 09:14:23 -0400

    Add adjlist to source

--- graph/src/graph.c
+++ graph/src/graph.c
@@ -2,12 +2,16 @@
 #include <stdint.h>
 
 struct graph_vertex {
-    int64_t vcount;
-    int64_t ecount;
+    void * p_data;
+
 };
 
 struct graph {
-
+    int64_t vcount;
+    int64_t ecount;
+    int8_t (* compare)(void * p_key1, void * p_key2);
+    void (* destroy)(void * p_data);
+    vertex ** pp_vertices;
 };
 
 graph * graph_init(void (* destory)(void * p_data), int8_t (* compare)(void * p_key1, void * p_key2));
@@ -16,6 +20,7 @@ vertex * graph_ins_vertex(graph * p_graph, void * p_data);
 int8_t graph_ins_edge(graph * p_grap, vertex * p_vertex1, vertex * p_vertex2);
 int8_t graph_rm_vertex(graph * p_graph, void * p_data); 
 int8_t graph_rm_edge(graph * p_graph, vertex * p_vertex1, vertex * p_vertex2);
+vertex ** graph_adjlist(graph * p_grap, vertex * p_vertex);
 int8_t graph_is_adjacent(graph * p_graph, vertex * p_vertex1, vertex * p_vertex2);
 int64_t graph_vcount(graph * p_graph);
 int64_t graph_ecount(graph * p_graph);
