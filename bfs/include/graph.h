e66781ed6327e02e9f3d624f5fb729566c857c88
Kevin Ellis <kevin.n.ellis3.mil@mail.mil>
2021-08-05 08:57:52 -0400

    Add graph data to the bfs directory

---
+++ bfs/include/graph.h
@@ -0,0 +1,17 @@
+#ifndef _GRAPH_H
+#define _GRAPH_H
+#include <list.h>
+#include <stdint.h>
+typedef struct graph graph;
+typedef struct graph_node vertex;
+graph * graph_init(void (* destroy)(void * p_data), int8_t (* compare)(void * p_key1, void * p_key2));
+void graph_destroy(graph * p_graph);
+vertex * graph_ins_vertex(graph * p_graph, void * p_data);
+int8_t graph_ins_edge(graph * p_graph, void * p_data1, void * p_void2);
+int8_t graph_rm_vertex(graph * p_graph, void * p_data); 
+int8_t graph_rm_edge(graph * p_graph, void * p_data1, void * p_data2);
+vertex ** graph_adjlist(graph * p_graph, void * p_data);
+int8_t graph_is_adjacent(graph * p_graph, void * p_data1, void * p_data2);
+int64_t graph_vcount(graph * p_graph);
+int64_t graph_ecount(graph * p_graph);
+#endif
---
+++ bfs/include/list.h
@@ -0,0 +1,22 @@
+#ifndef _LIST_H
+#define _LIST_H
+#include <stdint.h>
+typedef struct list_elem list_elem;
+typedef struct list list;
+
+list * list_init(void (* destroy)(void * data), int8_t (* compare)(void * key1, void * key2));
+void list_destroy(list * p_list_t);
+list_elem * list_ins_next(list * p_list_t, list_elem * p_elem_t, void * p_data);
+int8_t list_rm_next(list * p_list_t, list_elem * p_elem_t);
+int8_t list_iter(list * p_list_t, void (* func)(list_elem * elem));
+list_elem * list_search(list * p_list_t, void * data);
+void list_data_swap(list_elem * p_elem1, list_elem * p_elem2);
+void list_data_set(list_elem * p_elem, void * data);
+// getters
+uint16_t list_size (list * p_list_t);
+void * list_data(list_elem * p_elem);
+list_elem * list_head(list * p_list_t);
+int list_remove(list * p_list, void * p_data);
+list_elem * list_next(list_elem * p_elem);
+#endif
+
